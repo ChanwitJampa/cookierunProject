d9045fb5bfcd12f3b9db42bf695bda67656296bd
Chanwit <chanwit.jam@gmail.com>
2024-08-01 15:49:52 +0700

    update status code

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -3,7 +3,7 @@ import { ref, get } from "firebase/database"
 import db from '../config/connection.js'
 import jwt from 'jsonwebtoken'
 
-const login = (req, res, next) => {
+const login = async(req, res, next) => {
     const { username, password } = req.body;
 
 
@@ -12,40 +12,32 @@ const login = (req, res, next) => {
             throw new Error("username require")
         if (password === null || password === "")
             throw new Error("password require")
-        get(ref(db, 'users/' + username)).then((snapshot) => {
-            console.log(snapshot.val())
-            if (snapshot.exists()) {
-                console.log(`found user : ${username}  password is : ${snapshot.val().password}`)
-                if (Hash.compareHash(snapshot.val().password, password)) {
-                    const token = jwt.sign(
-                        { username: username },
-                        process.env.TOKEN_KEY, {
-                        expiresIn: "24h"
-                    })
-                    return res.status(200).json({
-                        RespCode: 200,
-                        RespMessage: 'login success',
-                        token:token
-                    })
-                } else {
-                    return res.status(200).json({
-                        RespCode: 500,
-                        RespMessage: "invalid username or password"
-                    })
-                }
-            } else {
-                return res.status(200).json({
-                    RespCode: 500,
-                    RespMessage: "invalid username or password"
+        const snapshot = await  get(ref(db, 'users/' + username))
+        // console.log(snapshot.val())
+        if (snapshot.exists()) {
+            console.log(`found user : ${username}  password is : ${snapshot.val().password}`)
+            if (Hash.compareHash(snapshot.val().password, password)) {
+                const token = jwt.sign(
+                    { username: username },
+                    process.env.TOKEN_KEY, {
+                    expiresIn: "24h"
                 })
+                return res.status(200).json({
+                    RespCode: 200,
+                    RespMessage: 'login success',
+                    token: token
+                })
+            } else {
+                res.status(400)
+                throw new Error("invalid username or password")
+
             }
-        }).catch((err2) => {
-            console.log("fail on get service err :" + err2)
-            return res.status(500).json({
-                RespCode: 500,
-                RespMessage: err2.message
-            })
-        })
+        } else {
+            res.status(400)
+            throw new Error("invalid username or password")
+
+        }
+
     } catch (err) {
         next(err)
     }
--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,7 +1,7 @@
 const errorHandler = (err,req,res,next)=>{
     const statusCode = res.statusCode ? res.statusCode : 500
 
-    res.status(200)
+    res.status(statusCode)
     res.json({
         RespCode:statusCode,
         stack:process.env.NODE_ENV === 'production'? null : err.stack
