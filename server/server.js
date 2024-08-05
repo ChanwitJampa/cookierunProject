56a0d1f5270c90d9d68555e81e00155f650a7c0f
Chanwit <chanwit.jam@gmail.com>
2024-08-05 19:50:12 +0700

    update

--- server/server.js
+++ server/server.js
@@ -19,10 +19,10 @@ dotenv.config();
 var app2 = express()
 app2.use(bodyParser.json())
 app2.use(bodyParser.urlencoded({ extended: true }))
-
-app2.use(cors({
-    origin: ['https://preview.construct.net','https://construct-arcade.com']
-}));
+app2.use(cors());
+// app2.use(cors({
+//     origin: ['https://preview.construct.net','https://construct-arcade.com']
+// }));
 // app2.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
